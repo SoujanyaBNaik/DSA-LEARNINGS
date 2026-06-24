b6f51a1f4ee4fa0c45a67611355d71cc529adb3f
SoujanyaNaik <78370817+SoujanyaBNaik@users.noreply.github.com>
2026-06-24 16:20:09 +0530

    Time: 4 ms (32.32%), Space: 10 MB (94%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    bool isSafe(vector<string>& board, int row, int col ,int n){
+        // horizontal check
+        for( int i = 0; i<n; i++){
+            if(board[row][i] == 'Q') return false;
+        }
+
+        //vertical check
+        for(int i=0 ; i< n ;i++){
+            if(board[i][col] == 'Q') return false;
+        }
+
+        //left diagonal
+        for( int i = row, j = col ; i>=0 && j>=0 ; i-- ,j--){
+            if ( board[i][j] == 'Q') return false;
+        }
+
+        // right diagonal
+        for( int i = row, j= col; i>=0 && j<n; i--, j++){
+            if(board[i][j] == 'Q') return false;
+        }
+        return true;
+    }
+
+    void helper(vector<string>& board,vector<vector<string>>& ans, int n, int row){
+
+        //base case
+        if(row == n){
+            ans.push_back(board);
+            return;
+        }
+
+        for( int j =0 ; j < n ; j++ ){
+            if(isSafe(board, row, j, n)){
+                board[row][j] = 'Q';
+                helper(board, ans, n, row+1);
+                board[row][j] = '.';
+            }
+        }
+        
+    }
+    vector<vector<string>> solveNQueens(int n) {
+        vector<string> board(n, string(n,'.'));
+        vector<vector<string>> ans;
+        helper( board, ans, n , 0);
+        return ans;
+
+    }
+};
